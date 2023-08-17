536168d1b31c7248c2c4c4cc36a6b161aefeb4d1
Frankie Leyva <frankieiii15@comcast.net>
2023-08-16 20:18:26 -0700

    Bullet sidebar list

--- components/sidebar.js
+++ components/sidebar.js
@@ -9,7 +9,7 @@ export default function Sidebar({ current, children }) {
   if(current === "Conditions"){targetlist = conditionlist;}else{targetlist = treatmentlist;}
     return <div className={styles.bar}>
       <Link className={styles.sectionWrapper} href={"/", current, "/", "learn"}><h1 className={styles.section}>{current}</h1></Link>
-      <div className={styles.linkList}>{targetlist.map((page) => {return(<Link  className={styles.link} href={"/", current, "/", page}>{page}</Link>)})}</div>
+      <div className={styles.linkList}>{targetlist.map((page) => {return(<Link  className={styles.link} href={"/", current, "/", page}>• {page}</Link>)})}</div>
     
     </div>
   }
