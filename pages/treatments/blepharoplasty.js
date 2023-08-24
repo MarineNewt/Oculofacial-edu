2e2ad0abde6203497446733b008cf611d76febe3
Frankie Leyva <frankieiii15@comcast.net>
2023-08-24 00:50:25 -0700

    Create Treatment template

--- pages/treatments/blepharoplasty.js
+++ pages/treatments/blepharoplasty.js
@@ -10,7 +10,16 @@ export default function FirstPost() {
         <title>Blepharoplasty Info</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className={styles.title}>Blepharoplasty</h1>
+      <div className={styles.primaryContent}>
+        <h1 className={styles.title}>Blepharoplasty</h1>
+        <div className={styles.qinfoContainer}>
+          <h4 className={styles.treatmentqinfo}>Type of Treatment: Surgical, Outpatient</h4>
+          <h4 className={styles.treatmentqinfo}>Commonly used to treat: <Link className={styles.inlineLink} href="/conditions/dermatochalasis">Dermatochalasis</Link></h4>
+        </div>
+        <h2 className={styles.secHeading}>Overview</h2>
+        <p className={styles.infoParagraph}>• Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+      <p className={styles.adviceInfo}>The information on this site should not be used as a substitute for professional medical care or advice. Contact a health care provider if you have questions about your health.</p>
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
