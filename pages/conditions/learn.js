c4db474d1d852cb04b8bbfd8be80991de6fcd1ab
Frankie Leyva <frankieiii15@comcast.net>
2023-08-26 00:05:14 -0700

    Update Learn pages

--- pages/conditions/learn.js
+++ pages/conditions/learn.js
@@ -10,7 +10,12 @@ export default function Learn() {
         <title>Conditions</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className={styles.title}>Identifying common oculofacial conditions.</h1>
+      <div className={styles.primaryContent}>
+        <h1 className={styles.title}>Identifying common oculofacial conditions.</h1>
+        <p className={styles.LearnInfoP}>Welcome to our informational oculoplastics resource, dedicated to shedding light on some of the most common oculofacial conditions and their signs. Being our window into the world and our initial presentation to those around us the oculofacial region is undeniably important. However, with age, genetics, and injury it is not uncommon for oculofacial conditions to arise causing serious impact to our every day routine.  From eyelid malpositions and tear duct disorders to orbital tumors and facial trauma, understanding these conditions is essential not only for preserving vision and ocular comfort but also for nurturing self-confidence and emotional well-being. By comprehending the intricate anatomy and function of this delicate region, we can better diagnose and treat various oculofacial conditions that might impact an individual's quality of life. Use the following materials to better familiarize yourself with oculofacial conditions empowering you with knowledge to make informed decisions about your eye health and appearance.</p>
+        <h2 className={styles.secHeading}>Most common conditions</h2>
+        <h3>Ptosis</h3><h3>Dermatochalasis</h3><h3>Chalazion</h3>
+      </div>
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
--- pages/treatments/learn.js
+++ pages/treatments/learn.js
@@ -10,9 +10,10 @@ export default function Learn() {
         <title>Conditions</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
+      <div className={styles.primaryContent}>
         <h1 className={styles.title}>Finding and understanding oculoplastic treatments.</h1>
-
+        <p className={styles.LearnInfoP}>Welcome to our guide on oculoplastic treatments, here you can explore a diverse array of interventions available to address a wide spectrum of oculofacial conditions. Oculoplastics treatments encompass a spectrum ranging from non-invasive procedures to intricate surgical techniques, all designed to restore function, alleviate discomfort, and enhance aesthetics. The approach to treating oculofacial conditions is highly personalized, considering factors such as the severity of the condition, the patient's unique anatomy, and their desired outcomes. For milder cases, conservative approaches like topical therapies or minimally invasive procedures might suffice. In more complex scenarios, surgical interventions such as eyelid reconstruction, orbital decompression, or tear duct repair might be necessary to achieve optimal results. Our aim is to provide you with comprehensive insights into the types of treatments available, ensuring that you are equipped with the knowledge needed to collaborate with your oculoplastic specialist in determining the most suitable treatment plan for your individual needs.</p>
+        <p className={styles.LearnInfoqtip}>Not sure where to start? Find corresponding treatments to specific <Link className={styles.none} href="/conditions/learn">conditions</Link> through their diagnosis page.</p>
       </div>
       <h2>
         <Link href="/">Back to home</Link>
