42d30641b33362253fa60a241b1f12c9564f1262
Frankie Leyva <frankieiii15@comcast.net>
2023-09-28 12:54:31 -0700

    Mobile Improvements

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,6 @@
 import styles from '../styles/PageLayout.module.css';
 import utilStyles from '../styles/utils.module.css';
+import Image from 'next/image'
 import Nav from './navbar.js'
 import Sidebar from './sidebar.js'
 import { useState } from 'react';
@@ -12,8 +13,8 @@ const sbiconClick = () => {
   return <>
     <Nav></Nav>
     <div className={styles.wrapper}>
-      <button className={styles.mobilesbbtn} style={{marginLeft: sbactive ? "50%" : "0"}} onClick={(event) => {sbiconClick()}}></button>
       <div className={styles.sidebar}><Sidebar current={current}></Sidebar></div>
+      <button className={styles.mobilesbbtn} style={{marginLeft: sbactive ? "50%" : "0"}} onClick={(event) => {sbiconClick()}}><Image src="/images/sidebar-arrow.png" width={400} height={400} className={styles.mobilesbicon} style={{transform: sbactive ? "scaleX(-1)" : "none"}}alt="Arrow icon"></Image></button>
       <div className={styles.mobilesidebar} style={{display: sbactive ? "block" : "none"}}><Sidebar current={current}></Sidebar></div>
       <div className={styles.content}>{children}</div>
       <div className={styles.additionals}>extra content</div>
