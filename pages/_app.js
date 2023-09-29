013ba61f757670846301b085566bfee33da96af0
Frankie Leyva <frankieiii15@comcast.net>
2023-09-29 00:05:08 -0700

    Adsense setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/globals.css';
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return <><Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4486350007861679"
+    crossorigin="anonymous"></Script><Component {...pageProps} /></>;
   }
